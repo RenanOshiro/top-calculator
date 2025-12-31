cf799ab0b4039bd156b8ad42c849634a501cd94f
Renan Kenji Oshiro <103371753+RenanOshiro@users.noreply.github.com>
2025-12-31 16:04:53 -0300

    Create function operate and program logic
    Created function operate handling each operator
    case and added program logic for each button type.
    Operator button still bugged when used to calculate instead of clicking equal button

--- script.js
+++ script.js
@@ -1,27 +1,96 @@
-function operate(n1, n2, op) {}
+function operate(n1, n2, op) {
+  let result = 0;
+  switch (op) {
+    case "+":
+      result = n1 + n2;
+      displayTop.textContent = `${number1} ${op} ${number2} =`;
+      displayBottom.textContent = result;
+      break;
 
-let number = "";
+    case "-":
+      result = n1 - n2;
+      displayTop.textContent = `${number1} ${op} ${number2} =`;
+      displayBottom.textContent = result;
+      break;
+
+    case "/":
+      result = n1 / n2;
+      displayTop.textContent = `${number1} ${op} ${number2} =`;
+      displayBottom.textContent = result;
+      break;
+
+    case "x":
+      result = n1 * n2;
+      displayTop.textContent = `${number1} ${op} ${number2} =`;
+      displayBottom.textContent = result;
+      break;
+
+    default:
+      result = n1;
+      displayTop.textContent = `${number1} ${op}`;
+      displayBottom.textContent = result;
+      break;
+  }
+  displayB = result;
+  number2 = null;
+}
+
+let displayB = "0",
+  displayT = "",
+  number1 = 0,
+  number2 = null,
+  op = "";
 const keyboard = document.querySelector(".keyboard");
 const displayBottom = document.querySelector(".display-bottom");
+const displayTop = document.querySelector(".display-top");
 
 keyboard.addEventListener("click", (event) => {
   // Verifica se o que foi clicado foi realmente um botão
   const target = event.target;
   const type = target.dataset.type;
+  const btnValue = target.innerText;
 
   if (target.tagName !== "BUTTON") return;
 
   switch (type) {
     case "number":
-      const valor = target.innerText;
+      if (displayB == "0") displayB = "";
+      if (number2 == null && op !== "") {
+        number2 = 0;
+        displayB = "";
+      }
+      displayB += btnValue;
+      displayBottom.textContent = displayB;
+      break;
 
-      if (number == "0") number = "";
-      number += valor;
-      displayBottom.textContent = number;
+    case "operator":
+      number1 = parseFloat(displayB);
+      op = btnValue;
+      if (number2 !== null) {
+        operate(number1, number2, op);
+      } else {
+        displayTop.textContent = `${number1} ${btnValue}`;
+      }
       break;
 
     case "clear":
-      number = "0";
-      displayBottom.textContent = number;
+      displayB = "0";
+      displayBottom.textContent = displayB;
+      displayT = "";
+      displayTop.textContent = displayT;
+      number1 = 0;
+      number2 = null;
+      op = "";
+      break;
+
+    case "equal":
+      if (op == "") {
+        number1 = parseFloat(displayB);
+        displayTop.textContent = `${number1} ${btnValue}`;
+      } else {
+        number2 = parseFloat(displayB);
+      }
+      operate(number1, number2, op);
+      break;
   }
 });
